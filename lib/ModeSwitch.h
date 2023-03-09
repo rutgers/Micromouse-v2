f0c7ad7c82a7a22bd829ebb642174558187a4fe4
EdwardgBenson <124222204+EdwardgBenson@users.noreply.github.com>
2023-03-08 21:10:53 -0500

    mode switch

---
+++ lib/ModeSwitch.h
@@ -0,0 +1,19 @@
+#ifndef MODESWITCH_H
+#define MODESWITCH_H
+
+#include <Arduino.h>
+
+#define pin_PWMA 6
+
+class ModeSwitch {
+
+    public: 
+        ModeSwitch();
+        void ModeState();
+        
+
+};
+
+
+
+#endif
